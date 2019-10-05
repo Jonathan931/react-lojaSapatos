b0600e76df4c399e9e7a034096d274f14db15aa4
Jonathan Matheus <jonathanmatheus@MacBook-de-Jonathan.local>
2019-10-05 14:18:21 -0300

    Updates

---
+++ precache-manifest.c5b3de564314dbdd7bfac7e79ae8c1a2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "754f10353005a5b67989fb7465cf6f7e",
+    "url": "/lojaSapatos/index.html"
+  },
+  {
+    "revision": "eab29175e0b7f51c39a2",
+    "url": "/lojaSapatos/static/css/2.39017165.chunk.css"
+  },
+  {
+    "revision": "eab29175e0b7f51c39a2",
+    "url": "/lojaSapatos/static/js/2.1f92f966.chunk.js"
+  },
+  {
+    "revision": "92a0110733b7d6a921b6",
+    "url": "/lojaSapatos/static/js/main.db641f67.chunk.js"
+  },
+  {
+    "revision": "8ae0103ce875c50a6a3a",
+    "url": "/lojaSapatos/static/js/runtime-main.aec99b4a.js"
+  },
+  {
+    "revision": "3c9a1ce505a48f2c15ba9505b6f5667b",
+    "url": "/lojaSapatos/static/media/background.3c9a1ce5.svg"
+  },
+  {
+    "revision": "879557c5bdbbd3743a7680c651e31d08",
+    "url": "/lojaSapatos/static/media/logo.879557c5.svg"
+  }
+]);
